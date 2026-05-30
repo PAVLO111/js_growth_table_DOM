98566a7eea2e7d6cea3ec23f7eb64c5b21861a5d
pavlo <nadazasada@gmail.com>
2026-05-30 18:37:22 +0300

    2 + 2 = 5

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,44 @@
 'use strict';
 
-// write code here 
+console.log('Hello World!');
+
+const appendRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+const appendColumn = document.querySelector('.append-column');
+const removeColumn = document.querySelector('.remove-column');
+const table = document.querySelector('.field');
+
+let rowState = document.querySelectorAll('tr').length;
+console.log(rowState);
+
+// let columnState = document.querySelector('tr').children.length;
+// let columnState = document.querySelectorAll('td').length / rowState;
+let columnState = 4;
+console.log(columnState);
+
+appendRow.addEventListener('click', (e) => {
+  const newRow = document.createElement('tr');
+
+  for (let i = 0; i < columnState; i++) {
+    const newRowCell = document.createElement('td');
+
+    newRow.append(newRowCell);
+  }
+
+  table.append(newRow);
+  rowState++;
+});
+
+// removeRow.addEventListener('click', (e) => {
+
+// });
+
+// appendColumn.addEventListener('click', (e) => {
+
+// });
+
+// removeColumn.addEventListener('click', (e) => {
+
+// });
+
+
