9fee0679dce2336dde406bc6d8e60ebfabd9615a
pavlo <nadazasada@gmail.com>
2026-05-30 19:58:22 +0300

    add events listeners

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,11 +9,12 @@ const removeColumn = document.querySelector('.remove-column');
 const table = document.querySelector('.field');
 
 let rowState = document.querySelectorAll('tr').length;
+
 console.log(rowState);
 
-// let columnState = document.querySelector('tr').children.length;
+let columnState = document.querySelector('tr').children.length;
 // let columnState = document.querySelectorAll('td').length / rowState;
-let columnState = 4;
+// const columnState = 4;
 console.log(columnState);
 
 appendRow.addEventListener('click', (e) => {
@@ -29,16 +30,21 @@ appendRow.addEventListener('click', (e) => {
   rowState++;
 });
 
-// removeRow.addEventListener('click', (e) => {
+removeRow.addEventListener('click', (e) => {
+  table.lastElementChild.remove();
+});
 
-// });
+appendColumn.addEventListener('click', (e) => {
+  let allRows = document.querySelectorAll('tr');
+  console.log(allRows);
 
-// appendColumn.addEventListener('click', (e) => {
+  const newCell = document.createElement('td');
 
-// });
+  allRows.forEach((row) => row.append(newCell));
+});
 
-// removeColumn.addEventListener('click', (e) => {
+removeColumn.addEventListener('click', (e) => {
+  let allRows = document.querySelectorAll('tr');
 
-// });
-
-
+  allRows.forEach((row) => row.lastChild.remove());
+});
